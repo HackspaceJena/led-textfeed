1572b865b6f352fb70c9747ce203c434a121db18
Sebastian Wagner <2000sw@gmail.com>
2015-02-23 22:48:08 +0100

    debug textfeed

--- textfeed.sh
+++ textfeed.sh
@@ -1,6 +1,22 @@
 #!/bin/sh
 
-echo -n "Y " \
- | convert -extract 16x16+43+43 -background  lightblue  -fill blue  -pointsize 21 text:-  bmp3:- \
- | dd bs=1 skip=54 \
- | nc -u 172.22.239.182 8888
+HOST="172.22.239.182"
+PORT=8888
+NC=$(which nc)
+
+DEBUG=0
+MESSAGE="Z"
+
+MESSAGE="$MESSAGE "
+if [ "$DEBUG" = 0 ]; then
+ echo -n "$MESSAGE" \
+  | convert -extract 16x16+43+43 -background  lightblue  -fill blue  -pointsize 21 text:-  bmp3:- \
+  | dd bs=1 skip=54 \
+  | "$NC" -u "$HOST" "$PORT"
+else
+ echo -n "$MESSAGE" \
+  | convert -extract 16x16+43+43 -background  lightblue  -fill blue  -pointsize 21 text:-  bmp3:- \
+  > /tmp/debug.bmp
+ gpicview /tmp/debug.bmp
+fi
+
